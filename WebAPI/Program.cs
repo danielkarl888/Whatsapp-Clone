5a4c92b586fe0b529bccd1abf30b404fed9f1df4
danielkarl888 <danielkarl888@gmail.com>
2022-05-10 23:56:48 +0300

    added allowance to react

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,13 +1,28 @@
 using Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Allow All",
+        builder =>
+        {
+            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        });
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Allow All",
+        builder =>
+        {
+            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        });
+});
 builder.Services.AddSingleton<IContactService, ContactService>();
 var app = builder.Build();
 
@@ -17,6 +32,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("Allow All");
 
 app.UseAuthorization();
 
